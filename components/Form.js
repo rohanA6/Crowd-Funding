00ce256ca16344b59bde9bc17cccb8190b85dd32
Rohan <rohanahire651@gmail.com>
2023-03-31 03:27:35 +0530

    campaign submit form done

---
+++ components/Form.js
@@ -0,0 +1,107 @@
+import React from "react";
+import FormField from "../components/FormField";
+import CustomButton from "./CustomButton";
+import { checkIfImage } from "../utils/index";
+
+const Form = ({ form, setForm }) => {
+  const handleFormFieldChange = (fieldName, e) => {
+    setForm({ ...form, [fieldName]: e.target.value });
+  };
+
+  //SubmitHandler
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    // checkIfImage(form.image, async (exists) => {
+    //   if (exists) {
+    //     setIsLoading(true);
+    //     await createCampaign({
+    //       ...form,
+    //       target: ethers.utils.parseUnits(form.target, 18),
+    //     });
+    //     setIsLoading(false);
+    //     navigate("/");
+    //   } else {
+    //     alert("Provide valid image URL");
+    //     setForm({ ...form, image: "" });
+    //   }
+    // });
+  };
+
+  return (
+    <form
+      onSubmit={submitHandler}
+      className="w-full mt-[65px] flex flex-col gap-[30px]"
+    >
+      <div className=" flex flex-wrap gap-[40px]">
+        <FormField
+          labelName="Your name *"
+          placeholder="name"
+          inputType="text"
+          value={form.name}
+          handleChange={(e) => handleFormFieldChange("name", e)}
+        />
+
+        <FormField
+          labelName="Campaign title *"
+          placeholder="write a title"
+          inputType="text"
+          value={form.title}
+          handleChange={(e) => handleFormFieldChange("title", e)}
+        />
+      </div>
+      <FormField
+        labelName="Story *"
+        placeholder="write your story"
+        isTextArea
+        value={form.description}
+        handleChange={(e) => handleFormFieldChange("description", e)}
+      />
+
+      <div className="w-full flex justify-start items-center p-4 bg-[#8c6dfd] h-[120px] rounded-[10px]">
+        <img
+          src="./assets/money.svg"
+          alt="money"
+          className="w-[40px] h-[40px] object-contain"
+        />
+        <h4 className="font-epilogue font-bold text-[25px] text-white ml-[20px]">
+          You will get 100% of the raised amount
+        </h4>
+      </div>
+
+      <div className=" flex flex-wrap gap-[40px]">
+        <FormField
+          labelName="Goal *"
+          placeholder="ETH 0.50"
+          inputType="number"
+          value={form.target}
+          handleChange={(e) => handleFormFieldChange("target", e)}
+        />
+
+        <FormField
+          labelName="End date *"
+          placeholder="write a title"
+          inputType="date"
+          value={form.deadline}
+          handleChange={(e) => handleFormFieldChange("deadline", e)}
+        />
+      </div>
+      <FormField
+        labelName="Campaign image *"
+        placeholder="uoload your campaign image URL"
+        value={form.image}
+        handleChange={(e) => handleFormFieldChange("image", e)}
+      />
+
+      <div className="flex justify-center items-center mt-[40px]">
+        <CustomButton
+          btnType="submit"
+          title="Submit new campaign"
+          styles="bg-[#1dc071]"
+        />
+      </div>
+    </form>
+  );
+};
+
+export default Form;
--- pages/createCampaigns.js
+++ pages/createCampaigns.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import Form from "../components/Form";
 
 const createCampaigns = () => {
-  return (
-    <div>createCampaigns</div>
-  )
-}
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+  const [form, setForm] = useState({
+    name: "",
+    title: "",
+    description: "",
+    target: "",
+    deadline: "",
+    image: "",
+  });
 
-export default createCampaigns
+
+  return (
+    <div className=" bg-[#1c1c24] flex justify-center items-center flex-col rounded-[10px] sm:p-10">
+      {isLoading && "Loading...."}
+
+      <div className=" flex justify-center items-center p-[16px] sm:min-w-[380px] bg-[#3a3a43] rounded-[10px]">
+        <h1 className=" font-epilogue font-bold sm:text-[25px] text-[18px] leading-[38px] text-white">
+          Start a Campaign 🚀
+        </h1>
+      </div>
+
+      {/* Submit form */}
+      <Form form={form} setForm={setForm} />
+    </div>
+  );
+};
+
+export default createCampaigns;
